2ecbefee488344dc2bae2a290ccc3652faa4674f
toneloke <guerrerotonyjs@gmail.com>
2017-10-17 10:51:33 -0400

    added authentication

--- controllers/cinemasController.js
+++ controllers/cinemasController.js
@@ -1,8 +1,29 @@
 const db = require('../models')
 
+// const findCinema = (err, cinema) => {
+//   console.log(res)
+//   if (err) res.json(err)
+//   if (!err) res.json(cinemas)
+// }
+
 module.exports = {
   getAll: function (req, res) {
     db.Cinema.find({})
+      .exec((err, cinemas) => {
+        if (err) res.json(err)
+        if (!err) res.json(cinemas)
+      })
+  },
+  getSingle: function (req, res) {
+    db.Cinema.find({_id: req.params.id})
+      .exec((err, cinema) => {
+        if (err) res.json(err)
+        if (!err) res.json(cinema)
+      })
+  },
+  search: function (req, res) {
+    let zip = req.query.zip
+    db.Cinema.find({ 'location.zip': { $lte: zip} })
       .exec((err, cinemas) => {
         if (err) res.json(err)
         if (!err) res.json(cinemas)
--- routes/cinemaRoutes.js
+++ routes/cinemaRoutes.js
@@ -3,9 +3,9 @@ const cinemasController = require('../controllers').cinemasController
 
 CinemasRouter.route('/cinemas')
   .get(cinemasController.getAll)
-// CinemasRouter.route('/cinemas/search')
-//   .get(cinemasController.search)
-// CinemasRouter.route('/cinemas/:id')
-//   .get(cinemasController.getSingle)
+CinemasRouter.route('/cinemas/search')
+  .get(cinemasController.search)
+CinemasRouter.route('/cinemas/:id')
+  .get(cinemasController.getSingle)
 
 module.exports = CinemasRouter
